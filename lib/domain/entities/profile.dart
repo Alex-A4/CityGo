8f4a223f5103547e3dbf0da86bde5788f5847058
= <alex.a4.25@yandex.ru>
2020-07-25 17:06:58 +0300

    Init profile

---
+++ lib/domain/entities/profile.dart
@@ -0,0 +1,62 @@
+import 'package:city_go/domain/entities/settings.dart';
+import 'package:city_go/domain/entities/user.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+/// Модель профиля, содержит информацию о пользователе, уведомлениях, языке и
+/// прочей информации, которая связана с профилем
+@immutable
+class Profile extends Equatable {
+  /// Объект пользователя, который может быть не инициализирован
+  static const USER = 'user';
+  final User user;
+
+  /// Объект настроек. Если при запуске приложения, нет данных о настройках, они
+  /// должны инициализироваться значениями по-умолчанию
+  static const SETTINGS = 'settings';
+  final Settings settings;
+
+  Profile({this.user, this.settings = const Settings()});
+
+  /// Фабрика по созданию профиля из JSON объекта
+  factory Profile.fromJson(Map<String, dynamic> json) {
+    if (json == null) return Profile.empty();
+
+    return Profile(
+      user: UserFactory.instance.fromJson(json[USER]),
+      settings: Settings.fromJson(json[SETTINGS]),
+    );
+  }
+
+  factory Profile.empty() => Profile(settings: Settings());
+
+  /// Метод для создания нового объекта.
+  /// Если входные значения не переданы (равны null), то будет использоваться
+  /// имеющееся значение.
+  Profile copyWith({User user, Settings settings}) {
+    return Profile(
+      user: user ?? this.user,
+      settings: settings ?? this.settings,
+    );
+  }
+
+  /// Метод для очистки какого-либо поля.
+  /// В случае настроек, это приведение их к настройкам по-умолчанию.
+  Profile clearField({bool user = false, bool settings = false}) {
+    return Profile(
+      user: user ? null : this.user,
+      settings: settings ? Settings() : this.settings,
+    );
+  }
+
+  /// Метод для конвертации объекта профиля в JSON формат.
+  /// Поскольку пользователь может отсутствовать, его значение может быть
+  /// равно null.
+  Map<String, dynamic> toJson() => {
+        USER: user?.toJson(),
+        SETTINGS: settings.toJson(),
+      };
+
+  @override
+  List<Object> get props => [user, settings];
+}
---
+++ test/domain/profile_test.dart
@@ -0,0 +1,154 @@
+import 'package:city_go/domain/entities/profile.dart';
+import 'package:city_go/domain/entities/settings.dart';
+import 'package:city_go/domain/entities/user.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final user = InAppUser(
+    userName: 'Вася',
+    accessToken: 'token',
+    type: UserType.InApp,
+  );
+  final settings = Settings(displayNotification: false);
+
+  group('fromJson', () {
+    test(
+      'должен создать профиль по-умолчанию, если json не передан',
+      () async {
+        // act
+        var profile = Profile.fromJson(null);
+
+        // assert
+        expect(profile.user, null);
+        expect(profile.settings, Settings());
+        expect(profile, Profile());
+      },
+    );
+
+    test(
+      'должен создать профиль из JSON объекта, где пользователь отсутствует',
+      () async {
+        // arrange
+        var json = {Profile.SETTINGS: settings.toJson()};
+
+        // act
+        var profile = Profile.fromJson(json);
+
+        // assert
+        expect(profile.user, null);
+        expect(profile.settings, settings);
+      },
+    );
+
+    test(
+      'должен создать профиль со всеми данными',
+      () async {
+        // arrange
+        var json = {
+          Profile.SETTINGS: settings.toJson(),
+          Profile.USER: user.toJson()
+        };
+
+        // act
+        var profile = Profile.fromJson(json);
+
+        // assert
+        expect(profile.user, user);
+        expect(profile.settings, settings);
+      },
+    );
+  });
+
+  group('toJson', () {
+    test(
+      'должен конвертировать профиль в JSON',
+      () async {
+        // arrange
+        var json = {
+          Profile.SETTINGS: settings.toJson(),
+          Profile.USER: user.toJson()
+        };
+
+        // act
+        var profile = Profile(settings: settings, user: user);
+
+        // assert
+        expect(profile.toJson(), json);
+      },
+    );
+
+    test(
+      'должен конвертировать в json, если нет пользователя',
+      () async {
+        // arrange
+        var profile = Profile(settings: settings);
+
+        // act
+        var json = profile.toJson();
+
+        // assert
+        expect(json[Profile.USER], null);
+        expect(json[Profile.SETTINGS], settings.toJson());
+      },
+    );
+  });
+
+  group('copyWith', () {
+    test(
+      'должен скопировать профиль с новыми настройками',
+      () async {
+        // act
+        var profile = Profile();
+        // assert
+        expect(profile.settings, isNot(settings));
+        profile = profile.copyWith(settings: settings);
+        expect(profile.settings, equals(settings));
+      },
+    );
+
+    test(
+      'должен скопировать профиль с новым пользователем',
+      () async {
+        // arrange
+        var profile = Profile();
+
+        // act
+        profile = profile.copyWith(user: user);
+
+        // assert
+        expect(profile.user, equals(user));
+      },
+    );
+  });
+
+  group('clearField', () {
+    test(
+      'должен очистить поле пользователя',
+      () async {
+        // arrange
+        var profile = Profile(user: user);
+        expect(profile.user, isNotNull);
+
+        // act
+        profile = profile.clearField(user: true);
+
+        // assert
+        expect(profile.user, null);
+      },
+    );
+
+    test(
+      'должен привести настройки к настройкам по-умолчанию',
+      () async {
+        // arrange
+        var profile = Profile(settings: settings);
+
+        // act
+        profile = profile.clearField(settings: true);
+
+        // assert
+        expect(profile.settings, equals(Settings()));
+      },
+    );
+  });
+}
