0144bdc55995ca154c8810da62a3626c951685d8
= <alex.a4.25@yandex.ru>
2020-07-25 15:51:27 +0300

    Init settings entity

---
+++ lib/domain/entities/settings.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+
+/// Класс, описывающий настройки приложения, которые могут изменяться.
+///
+/// Объекты настроек можно сравнивать с использованием оператора ==.
+/// При сравнении используются данные из [props].
+class Settings extends Equatable {
+  /// Флаг, определяющий, нужно ли показывать уведомления
+  static const DISPLAY_NOTIFICATION = 'notification';
+  final bool displayNotification;
+
+  /// Конструктор настроек.
+  /// Если какие-то параметры не указаны, они инициализируются значениями
+  /// по-умолчанию.
+  Settings({this.displayNotification = true});
+
+  /// Фабрика по созданию настроек из JSON объекта.
+  factory Settings.fromJson(Map<String, dynamic> json) {
+    if (json == null) return Settings(); // по-умолчанию
+
+    return Settings(displayNotification: json[DISPLAY_NOTIFICATION]);
+  }
+
+  /// Метод для создания нового объекта настроек на основе предыдущего и новых
+  /// значений, находящихся в [newData].
+  /// Ключами [newData] должны являться константы флагов,
+  /// например [DISPLAY_NOTIFICATION].
+  Settings copyWith(Map<String, dynamic> newData) {
+    return Settings(
+      displayNotification:
+          newData[DISPLAY_NOTIFICATION] ?? this.displayNotification,
+    );
+  }
+
+  /// Метод для конвертации настроек в JSON формат
+  Map<String, dynamic> toJson() => {
+        DISPLAY_NOTIFICATION: displayNotification,
+      };
+
+  @override
+  List<Object> get props => [displayNotification];
+}
---
+++ test/domain/settings_test.dart
@@ -0,0 +1,49 @@
+import 'package:city_go/domain/entities/settings.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'должен создать настройки по-умолчанию, если json отсутствует',
+    () async {
+      // act
+      var settings = Settings.fromJson(null);
+
+      // assert
+      expect(settings.displayNotification, true);
+      // Дополнительная проверка, которая включает в себя проверку всех полей.
+      // Эта проверка должна обновляться при расширении настроек
+      expect(settings.toJson(), equals({Settings.DISPLAY_NOTIFICATION: true}));
+    },
+  );
+
+  test(
+    'должен создать объект из json',
+    () async {
+      // arrange
+      var json = {Settings.DISPLAY_NOTIFICATION: false};
+
+      // act
+      var settings = Settings.fromJson(json);
+
+      // assert
+      expect(settings.displayNotification, json[Settings.DISPLAY_NOTIFICATION]);
+      // Дополнительная проверка, которая включает в себя проверку всех полей.
+      // Эта проверка должна обновляться при расширении настроек
+      expect(settings.toJson(), equals(json));
+    },
+  );
+
+  test(
+    'должен создать объект по-умолчанию через конструктор',
+    () async {
+      // act
+      var settings = Settings();
+
+      // assert
+      expect(settings.displayNotification, true);
+      // Дополнительная проверка, которая включает в себя проверку всех полей.
+      // Эта проверка должна обновляться при расширении настроек
+      expect(settings.toJson(), equals({Settings.DISPLAY_NOTIFICATION: true}));
+    },
+  );
+}
