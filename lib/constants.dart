8ff266f6ef5dbe6915ec4bca428961dc1954a50a
= <alex.a4.25@yandex.ru>
2020-09-01 18:11:41 +0300

    Add yaroslavl cords

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,5 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 const MAP_API_KEY = 'AIzaSyBQbZ1QyH70n-w3q5C2vA8v2hb6TnYGAcA';
+const CameraPosition kYaroslavlPos = CameraPosition(
+    target: LatLng(57.62967635359928, 39.8791329190135), zoom: 15);
