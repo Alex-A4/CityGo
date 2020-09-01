0d64535f4009b5fc2cf5f8f4acd44310ea7b01bf
= <alex.a4.25@yandex.ru>
2020-09-01 21:37:12 +0300

    Add constants

--- lib/data/core/localization_constants.dart
+++ lib/data/core/localization_constants.dart
@@ -2,3 +2,4 @@ const NO_INTERNET = 'no_internet';
 const UNEXPECTED_ERROR = 'unexpected_error';
 const USER_NOT_AUTH = 'user_not_auth';
 const LOCATION_ACCESS_DENIED = 'location_access_denied';
+const LOCATION_SERVICE_DISABLED = 'location_service_disabled';
