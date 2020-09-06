43b75b11feed5945fa7bf06046dc152613c033f6
= <alex.a4.25@yandex.ru>
2020-09-06 13:37:19 +0300

    Update api key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBQbZ1QyH70n-w3q5C2vA8v2hb6TnYGAcA")
+    GMSServices.provideAPIKey("AIzaSyD2k7DPFq9u8w2FcQgjtD1mzXoE9ULTEJc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-const MAP_API_KEY = 'AIzaSyBQbZ1QyH70n-w3q5C2vA8v2hb6TnYGAcA';
+const MAP_API_KEY = 'AIzaSyD2k7DPFq9u8w2FcQgjtD1mzXoE9ULTEJc';
 const CameraPosition kYaroslavlPos = CameraPosition(
     target: LatLng(57.62967635359928, 39.8791329190135), zoom: 15);
