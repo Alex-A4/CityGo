70f2f31b2d84904888c090874efd44bffcea15c2
= <alex.a4.25@yandex.ru>
2020-09-19 15:54:32 +0300

    Add starting route list, visit list. Root is main page

--- lib/app/navigator/router.dart
+++ lib/app/navigator/router.dart
@@ -1,6 +1,9 @@
+import 'package:city_go/app/pages/main_page/main_page.dart';
 import 'package:city_go/app/pages/path_map/path_map_page.dart';
 import 'package:city_go/app/pages/profile_auth/profile_auth_page.dart';
+import 'package:city_go/app/pages/route_list/route_list_page.dart';
 import 'package:city_go/app/pages/route_map/route_map_page.dart';
+import 'package:city_go/app/pages/visit_place_list/visit_list_page.dart';
 import 'package:city_go/data/core/service_locator.dart';
 import 'package:flutter/material.dart';
 
@@ -9,11 +12,14 @@ const ROOT = '/';
 const PATH_MAP_PAGE = '/path_map';
 const ROUTE_MAP_PAGE = '/route_map';
 const PROFILE = '/profile';
+const VISIT_LIST = '/visit_list';
+const ROUTE_LIST = '/route_list';
 
 /// Роуты, в которые не нужно передавать данные, они основаны на DI
 final routes = <String, WidgetBuilder>{
-  ROOT: (_) => null,
+  ROOT: (_) => MainPage(),
   PROFILE: (_) => ProfileAuthPage(bloc: sl()),
+  ROUTE_LIST: (_) => RouteListPage(bloc: sl()),
 };
 
 /// Роуты, в которые необходимо передавать данные.
@@ -31,6 +37,15 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       final route = settings.arguments;
       return MaterialPageRoute(
         builder: (_) => RouteMapPage(route: route),
+        settings: settings,
+      );
+    case VISIT_LIST:
+      final map = settings.arguments as Map;
+      return MaterialPageRoute(
+        builder: (_) => VisitListPage(
+          titleCode: map['title'],
+          bloc: sl.call(param1: map['type']),
+        ),
         settings: settings,
       );
     default:
