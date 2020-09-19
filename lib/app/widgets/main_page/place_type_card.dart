20c4699687f51386f96cb3ea07a6327a9a808334
= <alex.a4.25@yandex.ru>
2020-09-19 15:16:00 +0300

    Init card for main page

---
+++ lib/app/widgets/main_page/place_type_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:city_go/localization/localization.dart';
+
+/// Карточка главной страницы, на которой описано тип мест или пеших маршрутов.
+class PlaceTypeCard extends StatelessWidget {
+  final String imagePath;
+  final String nameCode;
+  final Function onTap;
+  final double height;
+
+  PlaceTypeCard({
+    Key key,
+    @required this.imagePath,
+    @required this.nameCode,
+    @required this.onTap,
+    @required this.height,
+  })  : assert(imagePath != null && nameCode != null && onTap != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).textTheme.headline4;
+
+    return Container(
+      margin: EdgeInsets.only(bottom: 3),
+      height: height - 3,
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage(imagePath), fit: BoxFit.cover),
+      ),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Center(
+          child: Text(context.localization(nameCode), style: style),
+        ),
+      ),
+    );
+  }
+}
