0c54852f663ec2df342120e106d29425037068d1
= <alex.a4.25@yandex.ru>
2020-09-24 21:54:51 +0300

    Init info appbar for single objects

---
+++ lib/app/general_widgets/info_app_bar.dart
@@ -0,0 +1,46 @@
+import 'package:city_go/app/general_widgets/adaptive_button.dart';
+import 'package:flutter/material.dart';
+
+/// Информационный бар, который содержит только заголовок и кнопку назад
+class InfoAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  InfoAppBar({Key key, @required this.title})
+      : assert(title != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Padding(
+        padding: EdgeInsets.only(left: 16,right: 16, top: 40, bottom: 16),
+        child: Row(
+          children: [
+            AdaptiveButton.orangeTransparent(
+              icon: Icons.arrow_back,
+              onTap: () => Navigator.of(context).pop(),
+            ),
+            Expanded(
+              child: Text(
+                title,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontFamily: 'Jost',
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.end,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => size;
+
+  static Size get size => Size.fromHeight(kToolbarHeight + 20);
+}
