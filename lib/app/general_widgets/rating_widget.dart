e8554f16812e67b63163bc5a0935cf9a125f12bc
= <alex.a4.25@yandex.ru>
2020-09-27 11:02:28 +0300

    Init rating widget

---
+++ lib/app/general_widgets/rating_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+/// Виджет рейтинга, который показывает 5 звезд и заполняет их в соответствии
+/// с нужным количеством в строку.
+class RatingWidget extends StatelessWidget {
+  final double rating;
+  static const top = 5;
+
+  RatingWidget({Key key, @required this.rating})
+      : assert(rating != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Количество заполненных звезд
+    final count = rating.floor();
+    // Количество пустых звезд
+    final empty = top - rating.ceil();
+    final needHalf = count + empty < top;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          ...List.generate(count, (index) => getIcon(Icons.star)),
+          if (needHalf) getIcon(Icons.star_half_sharp),
+          ...List.generate(empty, (index) => getIcon(Icons.star_border)),
+          Text(
+            rating.toStringAsFixed(1),
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontFamily: 'AleGray',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget getIcon(IconData icon) {
+    return Icon(icon, color: Colors.white, size: 30);
+  }
+}
