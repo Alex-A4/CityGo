84fe86e1f3c12fbb43c481f9e4d90480b0d82fa0
= <alex.a4.25@yandex.ru>
2020-09-27 12:45:15 +0300

    Init filter widget

---
+++ lib/app/widgets/visit_place_list/ui/filter_widget.dart
@@ -0,0 +1,73 @@
+import 'package:city_go/app/general_widgets/ui_constants.dart';
+import 'package:city_go/data/core/localization_constants.dart';
+import 'package:city_go/domain/repositories/visit_place/place_repository.dart';
+import 'package:city_go/localization/localization.dart';
+import 'package:flutter/material.dart';
+
+/// Виджет, отображающий кнопки для фильтрации
+class FilterWidget extends StatelessWidget implements PreferredSizeWidget {
+  final PlaceSortType activeType;
+  final Function(PlaceSortType) onTap;
+
+  FilterWidget({Key key, @required this.activeType, @required this.onTap})
+      : assert(activeType != null && onTap != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final top = MediaQuery.of(context).padding.top;
+    return SafeArea(
+      top: false,
+      child: Material(
+        color: Colors.grey[800],
+        elevation: 10,
+        child: Padding(
+          padding: EdgeInsets.only(top: top, bottom: 5),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 10, bottom: 5),
+                child: Text(
+                  context.localization(SORT_BY_WORD),
+                  style: TextStyle(
+                    color: Colors.grey[100],
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ...PlaceSortType.values.map((e) => sortButton(e, context)),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget sortButton(PlaceSortType type, BuildContext context) {
+    return FlatButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      onPressed: () => onTap(type),
+      color: type == activeType ? Colors.grey[500] : Colors.grey[100],
+      child: Text(
+        context.localization(type.sortName),
+        style: type == activeType ? selectedTextStyle : defaultTextStyle,
+      ),
+    );
+  }
+
+  static const defaultTextStyle =
+      TextStyle(color: Color(0xFF616161), fontFamily: 'AleGray', fontSize: 16);
+
+  final selectedTextStyle = defaultTextStyle.copyWith(color: orangeColor);
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight + 50);
+}
