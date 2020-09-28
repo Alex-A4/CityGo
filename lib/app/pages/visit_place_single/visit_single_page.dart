ed5c7018a6e61980ba5fe81d70a4da2b48458d08
= <alex.a4.25@yandex.ru>
2020-09-28 16:34:35 +0300

    Init visit single page

--- lib/app/pages/visit_place_single/visit_single_page.dart
+++ lib/app/pages/visit_place_single/visit_single_page.dart
@@ -1,18 +1,126 @@
+import 'package:city_go/app/general_widgets/description_widget.dart';
+import 'package:city_go/app/general_widgets/info_app_bar.dart';
+import 'package:city_go/app/general_widgets/toast_widget.dart';
+import 'package:city_go/app/general_widgets/ui_constants.dart';
 import 'package:city_go/app/widgets/visit_place_single/bloc/bloc.dart';
+import 'package:city_go/app/widgets/visit_place_single/ui/single_visit_content.dart';
 import 'package:city_go/domain/entities/visit_place/clipped_visit_place.dart';
+import 'package:city_go/domain/entities/visit_place/full_visit_place.dart';
 import 'package:flutter/material.dart';
 
 /// Экран для отображения информации о конкретном месте.
-class VisitSinglePage extends StatelessWidget {
+class VisitSinglePage extends StatefulWidget {
   final ClippedVisitPlace place;
   final VisitSingleBloc bloc;
 
-  VisitSinglePage({Key key, @required this.bloc, @required this.place})
-      : assert(bloc != null && place != null),
-        super(key: key);
+  VisitSinglePage({
+    Key key = const Key('VisitSinglePage'),
+    @required this.bloc,
+    @required this.place,
+  })  : assert(bloc != null && place != null),
+        super(key: key) {
+    bloc.add(VisitSingleBlocLoadEvent());
+  }
+
+  @override
+  _VisitSinglePageState createState() => _VisitSinglePageState();
+}
+
+class _VisitSinglePageState extends State<VisitSinglePage> {
+  VisitSingleBloc get bloc => widget.bloc;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: StreamBuilder<VisitSingleBlocState>(
+        stream: bloc,
+        initialData: bloc.state,
+        builder: (_, snap) {
+          final state = snap.data;
+          FullVisitPlace place;
+
+          if (state is VisitSingleBlocDataState) {
+            if (state.errorCode != null)
+              WidgetsBinding.instance.addPostFrameCallback(
+                  (_) => CityToast.showToast(context, state.errorCode));
+            place = state.place;
+          }
+          return LayoutBuilder(
+            builder: (context, constraints) {
+              final mq = MediaQuery.of(context);
+              final height = constraints.maxHeight -
+                  InfoAppBar.size.height -
+                  mq.padding.top -
+                  15;
+              final heightPercent = (height) / constraints.maxHeight;
+              return Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      // widget.client.getMediaPath(widget.clipped.image.path),
+                      widget.place.logo.path,
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Container(
+                  color: Color(0xBB000000),
+                  child: Stack(
+                    clipBehavior: Clip.hardEdge,
+                    fit: StackFit.expand,
+                    children: [
+                      Column(
+                        children: [
+                          InfoAppBar(title: widget.place.name),
+                          Divider(color: orangeColor, height: 1, thickness: 1),
+                          if (place == null) loadingIndicator,
+                          if (place != null)
+                            Expanded(
+                              child: SingleVisitContent(
+                                  place: place, bottomSize: height * 0.1),
+                            ),
+                        ],
+                      ),
+                      if (place != null)
+                        Positioned(
+                          left: 0,
+                          bottom: 0,
+                          child: Container(
+                            height: constraints.maxHeight,
+                            width: constraints.maxWidth,
+                            child: DraggableScrollableSheet(
+                              minChildSize: 0.1,
+                              maxChildSize: heightPercent,
+                              initialChildSize: 0.1,
+                              builder: (_, c) {
+                                return SingleChildScrollView(
+                                  child: DescriptionWidget(
+                                    description: place.description,
+                                    minHeight: height,
+                                    images: place.imageSrc,
+                                  ),
+                                  controller: c,
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
+
+  Widget get loadingIndicator => Expanded(
+        child: Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(orangeColor),
+          ),
+        ),
+      );
 }
