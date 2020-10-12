795fa89d0961b23e4f63ab2602e6c35f57b1c8ba
= <alex.a4.25@yandex.ru>
2020-10-12 10:38:20 +0300

    Add function that implements whole getting of location

--- lib/data/helpers/geolocator.dart
+++ lib/data/helpers/geolocator.dart
@@ -1,4 +1,7 @@
+import 'package:city_go/data/core/localization_constants.dart';
 import 'package:geolocator/geolocator.dart' as g;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+export 'package:geolocator/geolocator.dart';
 
 /// Поскольку плагин геолокатор ушел от использования класса в сторону методов,
 /// то реализован свой класс с теми же функциями.
@@ -8,16 +11,38 @@ abstract class Geolocator {
   Future<g.Position> getCurrentPosition();
 
   Future<g.LocationPermission> requestPermission();
+
+  /// Получение активной позиции с помощью [isLocationServiceEnabled] и
+  /// [getCurrentPosition].
+  /// Если возникают пробелмы, выбрасывает ошибку.
+  Future<LatLng> getPosition();
 }
 
 /// Геолокатор просто перенаправляет вызов на функции плагина
 class GeolocatorImpl extends Geolocator {
   @override
-  Future<g.Position> getCurrentPosition() => g.getCurrentPosition();
+  Future<g.Position> getCurrentPosition() =>
+      g.getCurrentPosition(timeLimit: Duration(seconds: 3));
 
   @override
   Future<bool> isLocationServiceEnabled() => g.isLocationServiceEnabled();
 
   @override
   Future<g.LocationPermission> requestPermission() => g.requestPermission();
+
+  @override
+  Future<LatLng> getPosition() async {
+    try {
+      bool isEnabled = await isLocationServiceEnabled();
+      if (isEnabled) {
+        var position = await getCurrentPosition();
+        return LatLng(position.latitude, position.longitude);
+      } else
+        throw LOCATION_SERVICE_DISABLED;
+    } on String catch (e) {
+      throw e;
+    } catch (_) {
+      throw LOCATION_ACCESS_DENIED;
+    }
+  }
 }
