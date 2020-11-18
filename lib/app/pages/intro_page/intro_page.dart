2809ab6c224d00814cf3fcde9a2d26426d2ef0ff
= <alex.a4.25@yandex.ru>
2020-11-18 09:35:25 +0300

    Replace flare to lottie

--- lib/app/pages/intro_page/intro_page.dart
+++ lib/app/pages/intro_page/intro_page.dart
@@ -1,23 +1,44 @@
 import 'package:city_go/app/navigator/router.dart';
-import 'package:flare_flutter/flare_actor.dart';
+import 'package:lottie/lottie.dart';
 import 'package:flutter/material.dart';
 
 /// Вступительный экран, содержащий анимацию.
 /// После завершения анимации, переходит на главную страницу.
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
   IntroPage({Key key}) : super(key: key);
+
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: Center(
-        child: FlareActor(
-          'assets/flare/intro.flr',
-          animation: 'Intro',
-          callback: (_) {
-            Navigator.of(context).pop();
-            Navigator.of(context).pushNamed(ROOT);
+        child: Lottie.asset(
+          'assets/lottie/gid.json',
+          controller: _controller,
+          onLoaded: (composition) {
+            _controller.addStatusListener((status) {
+              if (status == AnimationStatus.completed) {
+                Navigator.of(context).pop();
+                Navigator.of(context).pushNamed(ROOT);
+              }
+            });
+            _controller
+              ..duration = composition.duration
+              ..forward();
           },
           alignment: Alignment.center,
           fit: BoxFit.contain,
