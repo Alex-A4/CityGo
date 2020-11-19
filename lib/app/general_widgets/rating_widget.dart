bce41f25d3e92fc8068af24f7046a6e83c736402
= <alex.a4.25@yandex.ru>
2020-11-19 13:31:06 +0300

    Add tap function to rating widget

--- lib/app/general_widgets/rating_widget.dart
+++ lib/app/general_widgets/rating_widget.dart
@@ -6,9 +6,12 @@ import 'package:flutter/material.dart';
 /// с нужным количеством в строку.
 class RatingWidget extends StatelessWidget {
   final double rating;
+
+  /// Действие, которое позволяет перейти к простановке рейтинга
+  final Function(BuildContext) onTap;
   static const top = 5;
 
-  RatingWidget({Key key, @required this.rating})
+  RatingWidget({Key key, @required this.rating, @required this.onTap})
       : assert(rating != null),
         super(key: key);
 
@@ -22,23 +25,27 @@ class RatingWidget extends StatelessWidget {
 
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 20),
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          ...List.generate(count, (index) => getIcon(Icons.star)),
-          if (needHalf) getIcon(Icons.star_half_sharp),
-          ...List.generate(empty, (index) => getIcon(Icons.star_border)),
-          AutoSizeText(
-            rating.toStringAsFixed(1),
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontFamily: 'AleGray',
-              fontWeight: FontWeight.bold,
+      child: InkWell(
+        onTap: () => onTap(context),
+        borderRadius: BorderRadius.circular(10),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ...List.generate(count, (index) => getIcon(Icons.star)),
+            if (needHalf) getIcon(Icons.star_half_sharp),
+            ...List.generate(empty, (index) => getIcon(Icons.star_border)),
+            AutoSizeText(
+              rating.toStringAsFixed(1),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontFamily: 'AleGray',
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
